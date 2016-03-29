6d437e6e90a59d34937f9cea2c99946979f20fbf
ElliotShi <elliotshi@outlook.com>
2016-03-29 21:58:52 +0800

    added details page

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('myApp', []);
 
-app.controller('productList', ['$scope', '$http', '$location', function($scope, $http, $location) {
+app.controller('productList', ['$scope', '$http', function($scope, $http) {
 
   var products;
   var product;
@@ -16,71 +16,16 @@ app.controller('productList', ['$scope', '$http', '$location', function($scope, 
     for(var i = 0; i < products.length; i++){
       if(id == products[i].id){
         product = products[i];
-        $location.path('/products.html').replace();
+        if(window.localStorage){
+          localStorage.product = JSON.stringify(product);
+          window.location="/details.html";
+        }
       }
     }
   }
 
 }]);
 
-
-
-
-
-// notetwo.controller('noteControl', ['$scope', '$http', function($scope, $http){
-// 	console.log("hello from notetwo controller");
-
-// 	var refresh = function(){
-// 		$http.get('/note').success(function(response){
-// 			$scope.textareaEdit = true;
-// 			$scope.notelist = response;
-// 			$scope.note = $scope.notelist[0];
-// 		});
-// 	};
-
-// 	refresh();
-
-// 	$scope.create = function(){
-// 		$scope.note="";
-// 		$scope.p4Date=true;
-// 		$scope.div4Edit=true;
-// 		$scope.div4Save=true;
-// 		$scope.textareaEdit = false;
-// 	}
-
-// 	$scope.save = function(){
-// 			$http.post('/note', $scope.note).success(function(response){
-// 			console.log(response);
-// 			refresh();
-// 		});
-// 	}
-
-// 	$scope.show = function(id){
-// 		$http.get('/note/' + id).success(function(response){
-// 			$scope.p4Date=false;
-// 			$scope.div4Edit=false;
-// 			$scope.div4Save=false;
-// 			$scope.textareaEdit = true;
-// 			$scope.note = response;
-// 		})
-// 	}
-
-// 	$scope.edit = function(){
-// 		$scope.textareaEdit = false;
-// 	}
-
-// 	$scope.update = function(id){
-// 		$http.put('/note/' + id, $scope.note).success(function(response){
-// 			console.log(response);
-// 			refresh();
-// 		})
-// 	}
-
-// 	$scope.remove = function(id){
-// 		console.log(id);
-// 		$http.delete('/note/' + id).success(function(){
-// 			refresh();
-// 		})
-// 	}
-
-// }]);
+app.controller('productDetail', ['$scope', function($scope) {
+  $scope.product = JSON.parse(localStorage.product);
+}])
